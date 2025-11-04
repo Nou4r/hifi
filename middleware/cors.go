dfca658bae0e9bbda7be89abffb3102d4f034aee
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-04 23:06:05 +0800

    Remove CORS header setting for valid paths in middleware

--- middleware/cors.go
+++ middleware/cors.go
@@ -11,7 +11,6 @@ import (
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if slices.Contains(config.ValidPaths, r.URL.Path) {
-			w.Header().Set(config.HeaderAllowOrigin, config.CORSAllowOrigin)
 		}
 		next.ServeHTTP(w, r)
 	})
