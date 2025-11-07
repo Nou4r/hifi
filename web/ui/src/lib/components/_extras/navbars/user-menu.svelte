4d4fe7ba37009e7dd4683fc289fb6a274b3897cd
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-07 17:48:49 +0800

    refactor: update user menu to conditionally display sign out option and use guest username

--- web/ui/src/lib/components/_extras/navbars/user-menu.svelte
+++ web/ui/src/lib/components/_extras/navbars/user-menu.svelte
@@ -2,7 +2,7 @@
 	import Button from '$lib/components/ui/button.svelte';
 
 	import LogOutIcon from '@lucide/svelte/icons/log-out';
-	import { BookOpenText } from 'lucide-svelte';
+	import { BookOpenText, LogInIcon } from 'lucide-svelte';
 	import { Avatar, AvatarFallback, AvatarImage } from '$lib/components/ui/avatar';
 	import {
 		DropdownMenu,
@@ -14,6 +14,10 @@
 		DropdownMenuTrigger
 	} from '$lib/components/ui/dropdowns';
 	import ProfileIcon from '$lib/components/ProfileIcon.svelte';
+
+	let loggedIn = $state(false);
+
+	let guest = $state('Guest');
 </script>
 
 <DropdownMenu>
@@ -25,9 +29,9 @@
 				{...props}
 			>
 				<Avatar>
-					<AvatarImage src="" alt="Profile image" />
+					<!-- <AvatarImage src="" alt="Profile image" /> -->
 					<AvatarFallback>
-						<ProfileIcon username="k.kennedy" />
+						<ProfileIcon username={guest} />
 					</AvatarFallback>
 				</Avatar>
 			</Button>
@@ -35,7 +39,11 @@
 	</DropdownMenuTrigger>
 	<DropdownMenuContent class="max-w-64 bg-zinc-700" align="end">
 		<DropdownMenuLabel class="flex min-w-0  flex-col">
-			<span class="truncate text-sm font-medium text-zinc-100">Keith Kennedy</span>
+			{#if loggedIn}
+				<span class="truncate text-sm font-medium text-zinc-100">Keith Kennedy</span>
+			{:else}
+				<span class="truncate text-sm font-medium text-zinc-100">{guest}</span>
+			{/if}
 		</DropdownMenuLabel>
 		<DropdownMenuGroup>
 			<DropdownMenuSeparator class="bg-zinc-600" />
@@ -46,10 +54,19 @@
 				</DropdownMenuItem>
 			</a>
 		</DropdownMenuGroup>
+
+		{#if loggedIn}
+			<DropdownMenuSeparator class="bg-zinc-600" />
+			<DropdownMenuItem class="cursor-pointer text-zinc-100 focus:bg-zinc-600 focus:text-white">
+				<LogOutIcon size={16} class="opacity-80" aria-hidden="true" />
+				<span>Sign out</span>
+			</DropdownMenuItem>
+		{/if}
+
 		<DropdownMenuSeparator class="bg-zinc-600" />
 		<DropdownMenuItem class="cursor-pointer text-zinc-100 focus:bg-zinc-600 focus:text-white">
-			<LogOutIcon size={16} class="opacity-80" aria-hidden="true" />
-			<span>Sign out</span>
+			<LogInIcon size={16} class="opacity-80" aria-hidden="true" />
+			<span>Sign in</span>
 		</DropdownMenuItem>
 	</DropdownMenuContent>
 </DropdownMenu>
