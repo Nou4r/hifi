7b4f0947d61817187231fd74ae0751ce695dde00
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-07 08:16:23 +0800

    refactor: remove Logo export from navbar component

--- web/ui/src/lib/components/_extras/navbars/index.ts
+++ web/ui/src/lib/components/_extras/navbars/index.ts
@@ -1,7 +1,6 @@
 export { default as AppToggle } from './app-toggle.svelte';
 export { default as Filters } from './filters.svelte';
 export { default as InfoMenu } from './info-menu.svelte';
-export { default as Logo } from './logo.svelte';
 export { default as NotificationMenu } from './notification-menu.svelte';
 export { default as SettingsMenu } from './settings-menu.svelte';
 export { default as TeamSwitcher } from './team-switcher.svelte';
