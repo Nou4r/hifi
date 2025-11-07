ca4514c98a6bcc8a13a401b8b70c27221d430d82
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-07 21:05:01 +0800

    refactor: enhance Textarea component styling and remove unused InputGroup elements

--- web/ui/src/lib/components/Textarea.svelte
+++ web/ui/src/lib/components/Textarea.svelte
@@ -13,13 +13,18 @@
 </script>
 
 <div class="grid w-full max-w-sm gap-6">
-	<InputGroup.Root>
-		<InputGroup.Input placeholder="https://x.com/shadcn" readonly />
+	<InputGroup.Root class="border border-zinc-700 bg-zinc-900">
+		<InputGroup.Input
+			class="placeholder:text-green-300"
+			placeholder="https://x.com/shadcn"
+			readonly
+		/>
 		<InputGroup.Addon align="inline-end">
 			<InputGroup.Button
 				aria-label="Copy"
 				title="Copy"
 				size="icon-xs"
+				class="cursor-pointer hover:bg-zinc-300"
 				onclick={() => clipboard.copy('https://x.com/shadcn')}
 			>
 				{#if clipboard.copied}
@@ -28,38 +33,6 @@
 					<CopyIcon />
 				{/if}
 			</InputGroup.Button>
-		</InputGroup.Addon>
-	</InputGroup.Root>
-	<InputGroup.Root class="[--radius:9999px]">
-		<Popover.Root>
-			<Popover.Trigger>
-				{#snippet child({ props })}
-					<InputGroup.Addon>
-						<InputGroup.Button {...props} variant="secondary" size="icon-xs">
-							<InfoIcon />
-						</InputGroup.Button>
-					</InputGroup.Addon>
-				{/snippet}
-			</Popover.Trigger>
-			<Popover.Content align="start" class="flex flex-col gap-1 rounded-xl text-sm">
-				<p class="font-medium">Your connection is not secure.</p>
-				<p>You should not enter any sensitive information on this site.</p>
-			</Popover.Content>
-		</Popover.Root>
-		<InputGroup.Addon class="text-muted-foreground pl-1.5">
-			<InputGroup.Text>https://</InputGroup.Text>
-		</InputGroup.Addon>
-		<InputGroup.Input />
-		<InputGroup.Addon align="inline-end">
-			<InputGroup.Button onclick={() => (isFavorite = !isFavorite)} size="icon-xs">
-				<StarIcon class={isFavorite ? 'fill-blue-600 stroke-blue-600' : ''} />
-			</InputGroup.Button>
-		</InputGroup.Addon>
-	</InputGroup.Root>
-	<InputGroup.Root>
-		<InputGroup.Input placeholder="Type to search..." />
-		<InputGroup.Addon align="inline-end">
-			<InputGroup.Button variant="secondary">Search</InputGroup.Button>
 		</InputGroup.Addon>
 	</InputGroup.Root>
 </div>
