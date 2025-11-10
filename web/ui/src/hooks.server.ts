1d7fdd84ccc1f61b03b17287ad76f371e08de90a
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-11 04:54:36 +0800

    feat: add server-side handling with security headers and API_URL integration

---
+++ web/ui/src/hooks.server.ts
@@ -0,0 +1,14 @@
+import type { Handle } from '@sveltejs/kit';
+import { API_URL } from '$env/static/private';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const response = await resolve(event, {
+		filterSerializedResponseHeaders: (name) => name === 'content-type'
+	});
+
+	response.headers.set('Content-Security-Policy', 'script-src' + API_URL);
+
+	response.headers.set('Access-Control-Allow-Origin', API_URL);
+
+	return response;
+};
