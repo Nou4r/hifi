473c83895ab0cf9c0531cb60b08f19b35df35551
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-12 19:14:55 +0800

    fix: correct error message and update success message in signup action

--- web/ui/src/routes/+page.server.ts
+++ web/ui/src/routes/+page.server.ts
@@ -20,9 +20,11 @@ export const actions: Actions = {
 		});
 
 		if (!res.ok) {
-			return fail(res.status, { form, error: 'Signup failed' });
+			return fail(res.status, { form, error: 'Signin failed' });
 		}
 
-		return message(form, `Signup successful!`);
+		const data = await res.json();
+
+		return message(form, `Signin successful! Welcome ${data.host}`);
 	}
 };
