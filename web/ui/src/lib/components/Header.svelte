a9ba244fac21cf4032a0f89d75ec2c0ba92974ca
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-14 06:27:31 +0800

    fix: update condition for rendering connect account link in header

--- web/ui/src/lib/components/Header.svelte
+++ web/ui/src/lib/components/Header.svelte
@@ -75,7 +75,7 @@
 					>
 				</a>
 
-				{#if (user?.username && page.url.pathname != '/connect') || page.url.pathname == '/' || page.url.pathname.startsWith('/deactivate')}
+				{#if (user?.username && page.url.pathname == '/connect') || page.url.pathname == '/' || page.url.pathname.startsWith('/deactivate')}
 					<a
 						href="/connect"
 						aria-label="Connect account"
@@ -90,7 +90,6 @@
 					<a
 						href="/deactivate"
 						aria-label="Deactivate account"
-						data-sveltekit-reload
 						title="Deactivate"
 						class=" text-primary hover:text-primary/90 rounded-full p-3 hover:bg-zinc-600 hover:opacity-80 md:hidden"
 					>
