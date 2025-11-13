755ed93ba2952a8ec9682569634446e3e7159e5b
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-14 06:04:32 +0800

    fix: replace replaceState with pushState for navigation consistency

--- web/ui/src/routes/deactivate/+page.svelte
+++ web/ui/src/routes/deactivate/+page.svelte
@@ -5,7 +5,7 @@
 	import { Tabs, TabsContent, TabsList, TabsTrigger } from '$lib/components/ui/tabs';
 	import { Unplug, ShieldMinus, House } from 'lucide-svelte';
 	import Textarea from '$lib/components/Connect.svelte';
-	import { replaceState } from '$app/navigation';
+	import { pushState } from '$app/navigation';
 	import { page } from '$app/state';
 
 	let currentTab = 'tab-6';
@@ -22,7 +22,7 @@
 			<TabsList class="sticky top-6 flex w-80 flex-col gap-2 self-start bg-transparent p-4">
 				<TabsTrigger
 					value="tab-5"
-					onclick={() => replaceState('/connect', page.state)}
+					onclick={() => pushState('/connect', page.state)}
 					class="hidden w-full cursor-pointer items-center  justify-start gap-3 rounded-md px-6 py-4 text-3xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white md:flex"
 				>
 					<Unplug class="h-8 w-8" /> Connect
@@ -30,7 +30,7 @@
 
 				<TabsTrigger
 					value="tab-6"
-					onclick={() => replaceState('/deactivate', page.state)}
+					onclick={() => pushState('/deactivate', page.state)}
 					class="hidden w-full cursor-pointer items-center  justify-start gap-3 rounded-md px-6 py-4 text-3xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white md:flex"
 				>
 					<ShieldMinus class="h-8 w-8" /> Deactivate
