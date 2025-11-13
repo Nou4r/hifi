05fa844e89a49fc9b2233972710ac5397887a0af
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-13 23:17:24 +0800

    fix: update Subsonic admin password and add debug print statement in startLogin function

--- web/api/middleware/token.go
+++ web/api/middleware/token.go
@@ -41,6 +41,8 @@ func startLogin(ctx context.Context, client *http.Client, loginDoURL, user, pass
 
 		res := <-login
 
+		fmt.Println(string(res.Body))
+
 		if res.Err != nil {
 			token <- types.LoginResult{OK: false, Err: fmt.Errorf("invalid login: %d", res.Status)}
 			return
