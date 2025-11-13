c0b6a3ecac700ee4bad179505ae76411d96b42ac
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-14 02:31:23 +0800

    fix: update hover background style for Password.Copy component

--- web/ui/src/lib/components/Connect.svelte
+++ web/ui/src/lib/components/Connect.svelte
@@ -53,7 +53,7 @@
 							class="cursor-pointer hover:data-[state=off]:text-zinc-300 hover:data-[state=on]:text-zinc-300"
 						/>
 						<Password.Copy
-							class="cursor-pointer hover:bg-zinc-700 hover:text-zinc-200"
+							class="cursor-pointer hover:!bg-transparent hover:text-zinc-200"
 							aria-label="Copy Password"
 							title="Copy Password"
 						/>
