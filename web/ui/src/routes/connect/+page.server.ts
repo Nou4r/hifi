df0d022b6621534e60ebe8471ab2e81c210cfe28
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-15 03:33:03 +0800

    feat: add password validation in user deactivation action

--- web/ui/src/routes/connect/+page.server.ts
+++ web/ui/src/routes/connect/+page.server.ts
@@ -26,8 +26,16 @@ export const load: PageServerLoad = async (event) => {
 
 export const actions: Actions = {
 	default: async (e) => {
+		const { locals } = e;
+
 		const form = await superValidate(e, zod4(updateSchema));
 		if (!form.valid) return fail(400, { form });
+
+		if (locals.user?.password !== form.data.oldpassword) {
+			form.valid = false;
+			form.errors.oldpassword = ['Old password is required'];
+			return fail(400, { form });
+		}
 
 		const token = e.cookies.get('hifi');
 
