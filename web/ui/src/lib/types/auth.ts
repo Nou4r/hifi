acd6a6409a6eb5ed99799ceec84fb12f7374860c
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-15 00:47:33 +0800

    feat: add updateSchema for user authentication with username and password validation

--- web/ui/src/lib/types/auth.ts
+++ web/ui/src/lib/types/auth.ts
@@ -59,3 +59,61 @@ export const formSchema2 = z.object({
 			}
 		})
 });
+
+export const updateSchema = z.object({
+	username: z
+		.string()
+		.regex(/^[a-zA-Z0-9_]+$/, 'Username can only contain letters and numbers')
+		.trim()
+		.superRefine((val, ctx) => {
+			if (!val) {
+				ctx.addIssue({ code: 'custom', message: 'Username is required' });
+				return;
+			}
+			if (val.length < 2) {
+				ctx.addIssue({ code: 'custom', message: 'Username must be at least 2 characters long' });
+				return;
+			}
+			if (val.length > 50) {
+				ctx.addIssue({ code: 'custom', message: 'Username must not exceed 50 characters' });
+				return;
+			}
+		}),
+
+	oldpassword: z
+		.string()
+		.regex(/^[a-zA-Z0-9_@]+$/, 'Password can only contain letters and numbers')
+		.trim()
+		.superRefine((val, ctx) => {
+			if (!val) {
+				ctx.addIssue({ code: 'custom', message: 'Password is required' });
+				return;
+			}
+			if (val.length < 8) {
+				ctx.addIssue({ code: 'custom', message: 'Password must be at least 8 characters long' });
+				return;
+			}
+			if (val.length > 50) {
+				ctx.addIssue({ code: 'custom', message: 'Password must not exceed 50 characters' });
+				return;
+			}
+		}),
+	password: z
+		.string()
+		.regex(/^[a-zA-Z0-9_@]+$/, 'Password can only contain letters and numbers')
+		.trim()
+		.superRefine((val, ctx) => {
+			if (!val) {
+				ctx.addIssue({ code: 'custom', message: 'Password is required' });
+				return;
+			}
+			if (val.length < 8) {
+				ctx.addIssue({ code: 'custom', message: 'Password must be at least 8 characters long' });
+				return;
+			}
+			if (val.length > 50) {
+				ctx.addIssue({ code: 'custom', message: 'Password must not exceed 50 characters' });
+				return;
+			}
+		})
+});
