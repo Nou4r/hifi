d4b7506a0a218fe4d569d8d130c56b52415b12be
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-16 04:26:41 +0800

    refactor: move extractIDs function to a new file and implement GetNewAndTop function

---
+++ middleware/new_and_top.go
@@ -0,0 +1,22 @@
+package middleware
+
+func GetNewAndTop() []int {
+	ch := make(chan []int, 2)
+
+	go func() {
+		ch <- GetNew()
+	}()
+
+	go func() {
+		ch <- GetTop()
+	}()
+
+	var all []int
+	for range 2 {
+		ids := <-ch
+		all = append(all, ids...)
+	}
+	close(ch)
+
+	return all
+}
