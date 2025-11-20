ae64dff701a36971582c1dd41c3a4252e23ef6a7
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-11-20 18:59:21 +0800

    fix: update sizeMapping in cover function for accurate image sizes

--- middleware/cover.go
+++ middleware/cover.go
@@ -10,11 +10,15 @@ import (
 func cover(id string, size string, w http.ResponseWriter, r *http.Request) {
 	sizeMapping := map[int]int{
 		0:   750,
+		20:  80,
 		100: 160,
 		200: 320,
+		256: 320,
 		300: 320, // 750x750
 		450: 640, // 1080x1080
 		500: 750, // 1280x1280
+		512: 750,
+		600: 750,
 	}
 
 	s, _ := strconv.ParseInt(size, 10, 64)
